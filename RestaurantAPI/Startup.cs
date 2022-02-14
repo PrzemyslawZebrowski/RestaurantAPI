38de13eeb0c0310d4ed3a372593c81a0b69792ca
Przemysław Żebrowski <p.zebrowski30@gmail.com>
2022-02-14 20:34:01 +0100

    Add CORS policy

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -81,13 +81,20 @@ public class Startup
         services.AddScoped<IValidator<RestaurantQuery>, RestaurantQueryValidator>();
         services.AddHttpContextAccessor();
         services.AddSwaggerGen();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("FrontEndClient", builder =>
+                builder.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithOrigins(Configuration["AllowedOrigins"]));
+        });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSeeder seeder)
     {
+        app.UseCors("FrontEndClient");
         seeder.Seed();
-
         if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
         app.UseMiddleware<ErrorHandlingMiddleware>();
